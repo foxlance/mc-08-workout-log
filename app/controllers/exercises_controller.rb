f266812949f6c28f139c6ead531b02009418f5f7
Lance Vincent <lancevincent@gmail.com>
2015-08-31 17:22:12 +0800

    exercise form now saves

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,10 @@
 class ExercisesController < ApplicationController
+
+  def create
+    @workout = Workout.find(params[:workout_id])
+    @exercise = @workout.exercise.create(params.require(:exercise).permit(:name, :sets, :reps))
+
+    redirect_to workout_path(@workout), notice: "New exercise added!"
+  end
+
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,6 +1,6 @@
 class Workout < ActiveRecord::Base
 
-  has_many :exercise
+  has_many :exercise, dependent: :destroy
 
   validates :workout, :mood, :length, :date, presence: :true
 
